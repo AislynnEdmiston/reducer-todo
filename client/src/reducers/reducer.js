7440d82b210f50eee6b5a5161c0baf3b5ba11935
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-19 13:13:15 -0400

    Created initialState and reducer

---
+++ client/src/reducers/reducer.js
@@ -0,0 +1,19 @@
+export const initialState = [
+    {
+        item: 'Go Back To Denver',
+        completed: false,
+        id: 3892987589  
+    },
+    {
+        item: 'Get the house cleaned',
+        completed: false,
+        id: 3892987590  
+    }
+]
+
+export function reducer (state, action) {
+    switch (action.type){
+        default:
+            return state;
+    }
+}
